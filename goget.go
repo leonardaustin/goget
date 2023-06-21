65b7da088dcf2a5f567023170a9e6286334e829c
Leonard Austin <leonardaustin@users.noreply.github.com>
2023-06-21 12:54:27 +0100

    Update goget.go

--- goget.go
+++ goget.go
@@ -6,6 +6,7 @@ import (
 	"os"
 	"os/exec"
 	"path/filepath"
+	"strings"
 )
 
 func main() {
@@ -14,7 +15,7 @@ func main() {
 	}
 
 	repoURL := os.Args[1]
-	destPath := filepath.Join(os.Getenv("HOME"), "src", filepath.Base(repoURL))
+	destPath := getDestPath(repoURL)
 
 	err := gitClone(repoURL, destPath)
 	if err != nil {
@@ -24,15 +25,39 @@ func main() {
 	fmt.Println("Repository cloned and stored successfully!")
 }
 
+func getDestPath(repoURL string) string {
+	// Remove the protocol from the URL
+	repoURL = strings.TrimPrefix(repoURL, "https://")
+	repoURL = strings.TrimPrefix(repoURL, "http://")
+	repoURL = strings.TrimPrefix(repoURL, "git://")
+
+	// Split the remaining URL into parts
+	parts := strings.Split(repoURL, "/")
+
+	// Ensure the URL has at least 3 parts (e.g., github.com/orgname/repo)
+	if len(parts) < 3 {
+		log.Fatal("Invalid repository URL")
+	}
+
+	// Extract the domain and organization parts
+	domain := parts[0]
+	organization := parts[1]
+
+	// Create the destination path
+	destPath := filepath.Join(os.Getenv("HOME"), "src", domain, organization)
+
+	return destPath
+}
+
 func gitClone(repoURL, destPath string) error {
 	// Create the parent directory if it doesn't exist
-	err := os.MkdirAll(filepath.Dir(destPath), os.ModePerm)
+	err := os.MkdirAll(destPath, os.ModePerm)
 	if err != nil {
 		return err
 	}
 
 	// Set the command to execute
-	cmd := exec.Command("git", "clone", "https://"+repoURL, destPath)
+	cmd := exec.Command("git", "clone", repoURL, destPath)
 
 	// Execute the command
 	err = cmd.Run()
